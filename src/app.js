6de75e3791f466d368667c8ce77ec67930f21be2
zaziba.ahmed <ahmedzaziba@gmail.com>
2022-09-24 00:13:02 +0600

    Set up hbs partials and 404 page

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -17,20 +20,22 @@ app.use(express.static(publicDirectoryPath));
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
-        name: 'Zaziba'
+        name: 'Zaziba Ahmed'
     });
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About',
+        name: 'Zaziba Ahmed'
     });
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page',
-        helpText: 'Test kortesi'
+        helpText: 'Test kortesi',
+        name: 'Zaziba Ahmed'
     });
 })
 
@@ -41,6 +46,23 @@ app.get('/weather', (req, res) => {
         forecast: "It's too hot"
     });
 })
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Zaziba Ahmed',
+        errorMessage: 'Help article not found'
+    });
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Zaziba Ahmed',
+        errorMessage: 'Page not found'
+    });
+})
+
 
 app.listen(3000, () => {
     console.log('Server up on port 3000');
