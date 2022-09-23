2f1207e3f5824b47d8dda7d5835657bda40a348b
zaziba.ahmed <ahmedzaziba@gmail.com>
2022-09-23 23:16:08 +0600

    Practicing express

---
+++ src/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Zaziba'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        helpText: 'Test kortesi'
+    });
+})
+
+// first param = route
+app.get('/weather', (req, res) => {
+    res.send({
+        location: 'Dhaka',
+        forecast: "It's too hot"
+    });
+})
+
+app.listen(3000, () => {
+    console.log('Server up on port 3000');
+})
