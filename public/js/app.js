3f09d299775cbe6d7340abca81167a46ba24a3af
zaziba.ahmed <zaziba.ahmed@trucklagbe.com>
2022-09-25 18:16:03 +0600

    Created searh form and used fetch api

--- public/js/app.js
+++ public/js/app.js
@@ -6,14 +6,24 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
    }) 
 })
 
-fetch('http://localhost:3000/weather?address=Dhaka').then((response) => {
-   response.json().then((data)=>{
-        console.log(data);
-        if(data.error){
-            console.log(data.error);
-        }else{
-            console.log(data.location);
-            console.log(data.forecast);
-        }
-   }) 
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const location = search.value;
+    console.log(location);
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data)=>{
+                console.log(data);
+                if(data.error){
+                    console.log(data.error);
+                }else{
+                    console.log(data.location);
+                    console.log(data.forecast);
+                }
+        }) 
+     })
 })
