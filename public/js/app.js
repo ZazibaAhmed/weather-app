102ea49ebec5fdefa8741145a8197ddefa66f744
zaziba.ahmed <ahmedzaziba@gmail.com>
2022-09-25 23:43:31 +0600

    Weather app UI done

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,23 @@
-console.log('Testing app.js client side');
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-   response.json().then((data)=>{
-        console.log(data);
-   }) 
-})
-
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const location = search.value;
-    console.log(location);
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data)=>{
                 console.log(data);
                 if(data.error){
-                    console.log(data.error);
+                    messageOne.textContent = data.error;
                 }else{
-                    console.log(data.location);
-                    console.log(data.forecast);
+                    messageOne.textContent = 'Location: ' + data.location;
+                    messageTwo.textContent = 'Forecast: ' + data.forecast;
                 }
         }) 
      })
